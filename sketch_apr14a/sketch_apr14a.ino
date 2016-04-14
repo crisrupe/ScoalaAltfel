94cadf32806a99d48a7ebe49c001bf942abfca5c
crisrupe <crisporutiu@gmail.com>
2016-04-14 18:01:31 +0300

    Tema 14/04/2016

---
+++ sketch_apr14a/sketch_apr14a.ino
@@ -0,0 +1,87 @@
+void setup() {
+  for(int i=2;i<=12;i++)
+  {
+  pinMode(i, OUTPUT);
+   digitalWrite(i, HIGH);
+  delay(100);
+  }
+  for(int i=2;i<=12;i++)
+  {digitalWrite(i, LOW);
+  delay(100);
+  }
+}
+
+void loop() {
+  int n=12,j,i,n2,i2;
+  for(int i=0;i<=2;i++)
+  {
+  digitalWrite(2, HIGH);
+  delay(300);
+  digitalWrite(2, LOW);
+  delay(100);
+  }
+  for(int i=3;i<=n;i++)
+  { digitalWrite(i, HIGH);
+  delay(300);
+  digitalWrite(i, LOW);
+  delay(100);
+  }
+  
+  for(int i=3;i<=(n+2)/2;i++)
+  { digitalWrite(i, HIGH);
+     
+     digitalWrite((n+3-i),HIGH);
+     
+  }
+  for(int i=3;i<=(n+2)/2;i++)
+  { digitalWrite(i, LOW);
+     delay(500);
+     digitalWrite((n+3-i),LOW);
+     delay(500);
+  }
+    for(int i=3;i<=(n+2)/2;i++)
+  { digitalWrite((n+3-i),HIGH);
+     delay(250);
+    digitalWrite(i, HIGH);
+     delay(250);
+     
+  }
+  
+//  for(int i=3;i<=(n+2)/2;i++)
+//  { digitalWrite(n-5+i-3, LOW);
+//     delay(250);
+//     digitalWrite(n-5-i+3,LOW);
+//     delay(250);
+//  }
+
+  for(int i=3;i<=(n+4)/2;i++)
+  { digitalWrite(i, LOW);
+    delay(100);
+     digitalWrite(i+5,LOW);
+     delay(100);
+  }
+for(j=3;j<=12;j++)
+{for(i=3;i<=j;i++)
+{digitalWrite(i, HIGH);
+ delay(250);
+}
+n2=i;
+for(i2=i;i2>=3;i2--)
+{
+digitalWrite(i2, LOW);
+delay(100);
+}
+}
+
+
+
+
+
+
+    for(int i=2;i<=n;i++)
+  { digitalWrite(i, LOW);
+  delay(200);
+  }
+  
+  
+}
